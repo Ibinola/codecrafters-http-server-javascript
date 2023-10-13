92b0461f131bf44ecc7843129370d4a6abb29f33
Michael Ibinola <ibinolamichael1@gmail.com>
2023-10-13 23:43:10 +0100

    stage 3 fix

--- app/main.js
+++ app/main.js
@@ -5,10 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+  // socket.on("close", () => {
+  //   socket.end();
+  //   server.close();
+  // });
 
   socket.on("data", (buffer) => {
 
